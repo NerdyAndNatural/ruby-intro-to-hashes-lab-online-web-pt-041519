27dc2749a8e44d0f0e34d4895557f88a0b0afea1
Sarah Williams <srwilliams8678@gmail.com>
2019-04-26 01:17:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,8 +24,8 @@ def monopoly_with_second_tier
 	
 		monopoly = {
      :railroads => { 
-       :pieces => 4 
-       :names => { }
+       :pieces => 4,
+       :names => { },
        :rent_in_dollars => { }
        
      }
@@ -38,13 +38,13 @@ def monopoly_with_third_tier
 	
 	monopoly = {
      :railroads => { 
-       :pieces => 4 
+       :pieces => 4, 
        :names => { 
          :reading_railroad => { },
          :pennsylvania_railroad => { },
          :b_and_o_railroad  => { },
          :shortline => { },
-       }
+       },
        :rent_in_dollars => { 
          :one_piece_owned => 25,
           :two_pieces_owned => 50,
@@ -62,7 +62,7 @@ def monopoly_with_fourth_tier
 	
 	monopoly = {
      :railroads => { 
-       :pieces => 4 
+       :pieces => 4, 
        :names => { 
          :reading_railroad => { 
            "mortgage_value"=>"$100"
@@ -78,8 +78,8 @@ def monopoly_with_fourth_tier
            "mortgage_value"=>"$800"
            
          }
-       }
-       :rent_in_dollars =>{ 
+       },
+       :rent_in_dollars => { 
          :one_piece_owned => 25,
           :two_pieces_owned => 50,
           :three_pieces_owned => 100,
