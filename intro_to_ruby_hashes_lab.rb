eb7735deb2043f9010d08ceb9affebf2cd73a419
Sarah Williams <srwilliams8678@gmail.com>
2019-04-26 01:16:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -45,7 +45,7 @@ def monopoly_with_third_tier
          :b_and_o_railroad  => { },
          :shortline => { },
        }
-       :rent_in_dollars { 
+       :rent_in_dollars => { 
          :one_piece_owned => 25,
           :two_pieces_owned => 50,
           :three_pieces_owned => 100,
@@ -74,12 +74,12 @@ def monopoly_with_fourth_tier
            "mortgage_value"=>"$400" 
            
          },
-         :shortline=> {
+         :shortline => {
            "mortgage_value"=>"$800"
            
          }
        }
-       :rent_in_dollars { 
+       :rent_in_dollars =>{ 
          :one_piece_owned => 25,
           :two_pieces_owned => 50,
           :three_pieces_owned => 100,
